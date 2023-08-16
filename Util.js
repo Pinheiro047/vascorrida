e5ec1bb0074ee01969a098f3f8de144af7876fa4
mthzinn <matheusbenatti2@gmail.com>
2023-08-16 09:02:12 -0300

    arquivo util

---
+++ Util.js
@@ -0,0 +1,18 @@
+//classe base
+class obj{
+    constructor(x,y,w,h,attrib){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.attrib = attrib
+    }
+
+    dir = 0
+
+    des_obj(){
+        let img = new Image()
+        img.src = this.attrib
+        des.drawImage(img,this.x,this.y,this.w,this.h)
+}
+    }
